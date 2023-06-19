86f3d23fb562f52eda7dbb7c2ba66426456be47c
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-19 13:23:33 +0100

    hard-coded seattle added

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const seattle = {
+    minCust: 23,
+    maxCust: 65,
+    avgSales: 6.3,
+    customersEachHour: [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    cookiesEachHour: [16,20,35,48,56,77,93,144,119,84,61,23,42,57],
+    cookiesDaily: 875,
+};
+
+console.log(seattle);
