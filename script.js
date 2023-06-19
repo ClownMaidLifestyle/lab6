ecb53723fc88e5f6494446236a72a7332e1fda3e
Clownmaidlifestyle <e.edson@protonmail.ch>
2023-06-19 13:56:34 +0100

    generate seattle function working

--- script.js
+++ script.js
@@ -11,4 +11,30 @@ const seattle = {
 
 console.log(seattle);
 
-let salesContainer=document.getElementById("salesContainer");
+function generateSeattle(){
+    const salesContainer=document.getElementById("salesContainer");
+    console.log("bestie...");
+
+    const article = document.createElement("article");
+    salesContainer.appendChild(article);
+
+    const h3 = document.createElement("h3");
+    h3.textContent = "seattle";
+    article.appendChild(h3);
+
+    const ul = document.createElement("ul");
+    article.appendChild(ul);
+
+    for (let i = 0; i < seattle.cookiesEachHour.length; i++){
+        let li = document.createElement("li");
+        let suffix = "am";
+        let time = i + 6;
+        console.log(li);
+        if (time > 12){
+            suffix = "pm";
+            time = time - 12;
+        }
+        li.textContent = `${time}${suffix}: ${seattle.cookiesEachHour[i]}`
+        ul.appendChild(li);
+    }
+}
